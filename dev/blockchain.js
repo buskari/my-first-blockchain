f906c88cf847a88739f2f9b599fcd251008b9cd9
Andris Buscariolli <andris.contact@gmail.com>
2022-01-10 21:36:03 -0300

    add getLastBlock and hashBlock methods

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,20 +1,51 @@
-function Blockchain() {
-  this.chain = []
-  this.newTransactions = []
-}
+const sha256 = require('sha256')
 
-Blockchain.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
-  const newBlock = {
-    index: this.chain.length + 1,
-    timeStamp: Date.now(),
-    transactions: this.newTransactions,
-    nonce,
-    previousBlockHash,
-    hash
+class Blockchain {
+  constructor() {
+    this.chain = []
+    this.pendingTransactions = []
   }
 
-  this.newTransactions = []
-  this.chain.push(newBlock)
+  createNewBlock (nonce, previousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1, 
+      timeStamp: Date.now(),
+      transactions: this.pendingTransactions,
+      nonce,
+      previousBlockHash,
+      hash
+    }
+  
+    this.pendingTransactions = []
+    this.chain.push(newBlock)
+  
+    return newBlock
+  }
 
-  return newBlock
+  getLastBlock () {
+    return this.chain[this.chain.length - 1]
+  }
+
+  createNewTransacation (amount, sender, recipient) {
+    const newTransacation = {
+      amount,
+      sender,
+      recipient
+    }
+
+    this.pendingTransactions.push(newTransacation)
+    
+    return this.getLastBlock()['index'] + 1
+  }
+
+  hashBlock (
+    previousBlockHash,
+    currentBLockData,
+    nonce
+  ) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBLockData)
+    return sha256(dataAsString)
+  }
 }
+
+module.exports = Blockchain
