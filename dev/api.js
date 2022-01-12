1cce01a11a96a269f10a830a2fb231e484cea418
Andris Buscariolli <andris.contact@gmail.com>
2022-01-11 22:52:11 -0300

    add mine route

--- dev/api.js
+++ dev/api.js
@@ -1,25 +1,48 @@
 const express = require('express')
 const app = express()
 const Blockchain = require('./blockchain')
-const ethereum = new Blockchain()
+const { v4: uuidv4 } = require('uuid')
+
+const bitcoin = new Blockchain()
+const nodeAddress = uuidv4().split('-').join('')
 
 app.use(express.json())
 
+// get entire blockchain
 app.get('/blockchain', (req, res) => {
-  res.send(ethereum)
+  res.send(bitcoin)
 })
 
+// create a new transaction
 app.post('/transaction', (req, res) => {
   const { amount, sender, recipient } = req.body
-  const blockIndex = ethereum.createNewTransacation(amount, sender, recipient)
+  const blockIndex = bitcoin.createNewTransacation(amount, sender, recipient)
   
   res.json({
     note: `Transaction will be added in block ${blockIndex}.`
   })
 })
 
+// mine a block
 app.get('/mine', (req, res) => {
-  // ... mine a new block
+  const lastBlock = bitcoin.getLastBlock()
+  const previousBlockHash = lastBlock['hash']
+  const currentBLockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock['index'] + 1
+  }
+
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBLockData)
+  const hash = bitcoin.hashBlock(previousBlockHash, currentBLockData, nonce)
+
+  bitcoin.createNewTransacation(12.5, '00', nodeAddress)
+
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, hash)
+
+  res.json({
+    note: 'New block mined successfully',
+    block: newBlock
+  })
 })
 
 
